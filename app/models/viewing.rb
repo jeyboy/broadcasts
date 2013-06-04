b7f9a08e9fbb3d822c96ff58da6186d5f55d68e7
Jenua Boiko <jeyboy1985@gmail.com>
2013-06-04 14:46:22 -0700

    update viewing model

--- app/models/viewing.rb
+++ app/models/viewing.rb
@@ -1,7 +1,17 @@
 class Viewing < ActiveRecord::Base
+  attr_accessible :broadcast_id, :viewer_id, :viewer_type, :hidden_at
+
   belongs_to :broadcast, counter_cache: :viewings_count
   belongs_to :viewer, polymorphic: true
 
-  #TODO: Need testing on counter by condition !!!
-  belongs_to :broadcast, counter_cache: :hidden_viewings_count, conditions: 'hidden_at <> NULL'
+  validates :broadcast_id, presence: true
+
+  after_save :proceed_hidden_counter, if: :hidden_at_changed?
+
+protected
+  def proceed_hidden_counter
+    broadcast.update_attributes do |obj|
+      obj.hidden_viewings_count += 1
+    end
+  end
 end
