e6d49868bf78f74508376636b29e3533c9dd6091
Jenua Boiko <jeyboy1985@gmail.com>
2013-06-04 13:13:08 -0700

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Broadcasting::Engine.routes.draw do
   scope module: 'broadcasting' do
-    resources :broadcasts, only: [:index, :destroy]
+    #resources :broadcasts, only: [:index, :destroy]
+    match 'broadcasts_list' => 'broadcasts#index', via: :post, as: 'broadcasts_list'
+    match 'close_broadcast/:id' => 'broadcasts#destroy', via: :delete, as: 'close_broadcast'
   end
 end
